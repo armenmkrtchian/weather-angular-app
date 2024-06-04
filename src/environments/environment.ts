735da42a2a7426a21d3973653f417d2acafbe92b
armenmkrtchian <armenmkrtchian93@gmail.com>
2024-06-04 20:27:34 +0400

    Change API keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.openweathermap.org/data/2.5/',
-  apiKey: '74a48a4f3336846046b19fee6dd16c89',
+  apiUrl: 'https://api.openweathermap.org/data/3.0/',
+  apiKey: '15558304432ae1fa4a49c4ef34a0f5a5',
 };
