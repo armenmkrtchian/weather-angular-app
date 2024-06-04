915294b6c23fd4afc7351b476c86c8437a7c7e9e
armenmkrtchian <armenmkrtchian93@gmail.com>
2024-06-05 01:30:57 +0400

    Return the API key, because of issue with openweathermap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://api.openweathermap.org/data/2.5/',
-    apiKey: '15558304432ae1fa4a49c4ef34a0f5a5',
+    apiKey: '74a48a4f3336846046b19fee6dd16c89',
   };
   
